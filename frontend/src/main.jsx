ad717550d51eb3816b8c3baa6625e6444c90604e
1austineogenga <austineogenga.lakezone@gmail.com>
2026-07-05 02:31:38 +0300

    set status bar style programmatically at app startup for reliable APK behavior

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Capacitor } from '@capacitor/core'
+
+if (Capacitor.isNativePlatform()) {
+  import('@capacitor/status-bar').then(({ StatusBar, Style }) => {
+    StatusBar.setStyle({ style: Style.Dark })
+    StatusBar.setBackgroundColor({ color: '#1a2332' })
+  }).catch(() => {})
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
